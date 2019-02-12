c44d36bb266fa2ba6efa54b002f9e69ce3854195
faizan <faizan.tahir@digitalnet.com>
2019-02-12 14:49:43 +0500

    hoc

--- src/components/addButton.jsx
+++ src/components/addButton.jsx
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
-import hoverable from "./hover_hoc"
+import hoverable from "./hover_hoc";
 
 class AddButton extends Component {
-
-
   render() {
-	  let {isHovered,onClick,onMouseEnter,onMouseLeave} = this.props;
-	
+    let { isHovered, onClick, onMouseEnter, onMouseLeave } = this.props;
+
     return (
- 
-		  
-            <div className="kanban  add_card " style={isHovered?{background:"aliceblue"}:{}} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
-              <span>
-                <i style={isHovered?{background:"white"}:{}} className="fa fa-plus add_icon" aria-hidden="true" />
-                <b className="details">Add team member to this team</b>
-              </span>
-            </div>
-
-
+      
+      <div
+        className="kanban  add_card "
+        style={isHovered ? { background: "aliceblue" } : {}}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        onClick={onClick}
+      >
+        <span>
+          <i
+            style={isHovered ? { background: "white" } : {}}
+            className="fa fa-plus add_icon"
+            aria-hidden="true"
+          />
+          <b className="details">Add team member to this team</b>
+        </span>
+      </div>
     );
   }
-  
 }
 
 export default hoverable(AddButton);
